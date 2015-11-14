08bf4310ba393b46abfd3762a1af37e071ddd47c
pengliang <990607342@qq.com>
2015-11-14 15:28:56 +0800

    修正希尔排序

--- src/hust/sort/ShellSort.java
+++ src/hust/sort/ShellSort.java
@@ -21,7 +21,7 @@ public class ShellSort {
 	public static void shellSortMethod(int[] a, int N) { //增量序列的一种流行（但是不好）的选择是使用shell建议的序列：increment = N/2, increment/=2;
 		int increment=0, i=0, j=0;
 		for (increment = N/2; increment > 0; increment/=2) {
-			for (i=increment; i < N; i+=increment) { //分组进行插入排序
+			for (i=increment; i < N; i++) { //分组进行插入排序
 				int guard = a[i];
 				for (j=i-increment; j>=0 && a[j] > guard; j-=increment) {
 					a[j+increment] = a[j];
