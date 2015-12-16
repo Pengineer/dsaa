0ad67a96dccb1af3eaf275539d69b4789e663fa4
pengliang <990607342@qq.com>
2015-12-16 23:41:42 +0800

    add FindSecondBigNum

---
+++ src/hust/other/FindSecondBigNum.java
@@ -0,0 +1,30 @@
+package hust.other;
+
+/**
+ * 找到无序数组中第二大的数
+ * 
+ * @author 2015-12-15
+ *
+ */
+public class FindSecondBigNum {
+
+	public static void main(String[] args) {
+		int[] arr = {6,3,6,7,2,9,9};
+		System.out.println(find(arr));
+	}
+	
+	public static int find(int[] arr) {
+		int max = arr[0];
+		int sec_max = Integer.MIN_VALUE;
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] > max) {
+				sec_max = max;
+				max = arr[i];
+			} else if (arr[i] < max && arr[i] > sec_max) {
+				sec_max = arr[i];
+			}
+		}
+		
+		return sec_max;
+	}
+}
