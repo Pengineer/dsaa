9b03f70015c310ee91537e195abbc59835afcc2a
pengliang <990607342@qq.com>
2016-01-29 16:21:30 +0800

    add MaxIntegratableSubArray and RemoveNode

---
+++ src/hust/MaxIntegratableSubArray.java
@@ -0,0 +1,46 @@
+package hust;
+
+import java.util.HashSet;
+
+/**
+ * 先给出可整合数组的定义： 如果一个数组arr在排序之后，从最小值到最大值的顺序中，每相邻两个数之间差的绝对值都为1，则arr为可整合数组。 
+ * 例如： arr = {5，3，4，6，2}，再排序之后为：{2，3，4，5，6}，排序后符合每相邻两个数之间差的绝对值都为1，所以arr是可整合数组。 
+ * 
+ * 给定一个整形数组arr，请返回其中长度最大的可整合子数组的长度。
+ * [5,0,1,2,4,3,9]，最长可整合子数组为[5,0,1,2,4,3]，所以返回6
+ * [6,7,3,0,1,2,4,7]，最长可整合子数组为[3,0,1,2,4]，所以返回5
+ * 要求：如果数组长度为N，时间复杂度请达到O(N^2)
+ * 
+ * @author liangjian
+ * @since 2016-01-29
+ *
+ */
+public class MaxIntegratableSubArray {
+
+	//观察可整合数组的特点：1，没有重复元素；2，最大值-最小值+1=数组的长度
+	public static int getMaxLength(int[] arr) {
+		HashSet<Integer> set = new HashSet<Integer>();// hash表判断重复元素（哈希表的增删操作时间复杂度可看做O(1)）
+		int len =0;
+		for(int i=0; i<arr.length; i++) { // 以i为起点，往后扩
+			int max =Integer.MIN_VALUE;
+			int min =Integer.MAX_VALUE;
+			for(int j=i; j<arr.length; j++) {
+				if(set.contains(arr[j])) { // 满足特性1
+					break;
+				}
+				max = Math.max(max, arr[j]);
+				min = Math.min(min, arr[j]);
+				if(max - min == j - i) { // 满足特性2
+					len = Math.max(len, j- i + 1);
+				}
+			}
+			set.clear(); // 每一次外层遍历都需要清空集合
+		}
+		return len;
+	}
+	
+	public static void main(String[] args) {
+		int[] arr = {5,0,1,2,4,3,9};
+		System.out.println(getMaxLength(arr));
+	}
+}
---
+++ src/hust/link/RemoveNode.java
@@ -0,0 +1,65 @@
+package hust.link;
+
+/**
+ * 一种怪异的节点删除方式
+ * 【题目】
+ * 链表节点值类型为int型，给定一个链表中的节点node，但不给定整个链表的头节点，如何在链表中删除node？
+ * 请实现这个函数，并分析这么会出现哪些问题。
+ * 
+ * 要求时间复杂度为O(1)。
+ * 
+ * 【问题】
+ * 1，无法删除最后一个结点。比如说最后一个结点是node3，我们的目的就是让node2.next=null（注意了，node2.next指向的是node3指向的对象，与node3这个引用
+ *    没有半毛钱关系，因此node3=null没意义），那能不能将node3所指向的对象所在的区域变成null呢？不能，null在系统中是一个特性的区域，如果想让node2的
+ *    next指针指向null，必须找到node2。
+ * 2，这种删除方式在本质上根本就不是删除了node结点，而是把node结点的值改变，然后删除node的下一个结点，在实际的工程中可能会带来很大的问题。比如，工程上
+ *    的一个结点可能代表很复杂的结构，节点值的复制会相当复杂，或者可能改变节点值这个操作都是被禁止的；再如，工程上的一个结点代表提供服务的一个服务器，
+ *    外界对每个结点都有很多依赖，比如，示例中删除节点2时，其实影响了节点3对外提供的服务。
+ * 
+ * @author liangjian
+ * @since 2016-01-29
+ *
+ */
+public class RemoveNode {
+
+	public static void remove(Node node) {
+		if(node == null) {
+			return;
+		}
+		Node next = node.next;
+		if(next == null) {
+			throw new RuntimeException("Cannot delete the last one");
+//			node = null; 原引用置空没用，前面结点的next引用的指向与node无关
+//			return;
+		}
+		
+		node.value = next.value;
+		node.next = next.next;
+		next = null;
+	}
+	
+	public static void printLinkedList(Node head) {
+		System.out.print("Linked List: ");
+		while (head != null) {
+			System.out.print(head.value + " ");
+			head = head.next;
+		}
+		System.out.println();
+	}
+	
+	public static void main(String[] args) {
+		Node node1 = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+		Node node4 = new Node(4);
+		Node node5 = new Node(5);
+		node1.next = node2;
+		node2.next = node3;
+		node3.next = node4;
+		node4.next = node5;
+		node5.next = null;
+		printLinkedList(node1);
+		remove(node3);
+		printLinkedList(node1);
+	}
+}
