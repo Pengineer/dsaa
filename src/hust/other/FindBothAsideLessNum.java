db1e5bec70f664b49a07a416f435c3a716fb02bb
pengliang <990607342@qq.com>
2016-01-31 14:29:20 +0800

    simple modify

--- src/hust/other/FindBothAsideLessNum.java
+++ src/hust/other/FindBothAsideLessNum.java
@@ -9,4 +9,12 @@ package hust.other;
  */
 public class FindBothAsideLessNum {
 
+	/*
+	 * 要分别找到一个元素左右离自己最近的最小元素，可以借助栈，栈中元素升序排列，一旦遇到不和规则的，就弹出，直到符合规则，如果栈中还有元素，
+	 * 那么栈底元素就是左边最小值；在找右边最小时，清空栈，同理。（有点不对劲。。。）
+	 */
+	public static int[] getLessAside(int[] arr) {
+		
+		return null;
+	}
 }
