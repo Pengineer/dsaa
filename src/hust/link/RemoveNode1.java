e7063aa2ee5d75263b8b11c2504744d8abd1334b
pengliang <990607342@qq.com>
2016-04-12 19:55:49 +0800

    fix bug

--- src/hust/link/RemoveNode1.java
+++ src/hust/link/RemoveNode1.java
@@ -9,7 +9,14 @@ package hust.link;
 public class RemoveNode1 {
 
 	public static void main(String[] args) {
-		
+		Node node1 = new Node(1);
+		Node node2 = new Node(2);
+		Node node3 = new Node(3);
+		Node node4 = new Node(1);
+		Node node5 = new Node(5);
+		node1.next = node2;node2.next = node3;node3.next = node4;node4.next = node5;node5.next = null;
+		print(node1);
+		print(removeNode(node1, 1));
 	}
 	
 	/*
@@ -29,19 +36,22 @@ public class RemoveNode1 {
 			return node1;
 		}
 		
-		while(node2.next != null) {
-			if(node2.next.value != val) {
-				node1 = node2;
-				node2 = node2.next;
-			}
+		while(node2 != null && node2.value != val) {
+			node1 = node2;
+			node2 = node2.next;
 		}
 		
-		if(node2.next != null && node2.next.value == val) { //不是最后一个结点
-			node2.next = node2.next.next;
-		} else if(node2.next == null && node2.next.value == val){ //最后一个结点
-			node1.next = null;
+		if(node2 != null && node2.value == val) {
+			node1.next = node2.next;
 		}
 		return head;
 	}
 	
+	public static void print(Node head) {
+		while(head != null) {
+			System.out.println(head.value);
+			head = head.next;
+		}
+		System.out.println("------------------------------");
+	}
 }
